19e92aa161d2845e879b252035beba8d8f3d835a
dima <zhuikovda@gmail.com>
2022-12-02 22:39:45 +0300

    Accessing_Nested_Objects

---
+++ js/Accessing_Nested_Objects.js
@@ -0,0 +1,13 @@
+onst myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
